3b6a57ecf603dfd53b4a99cb7eb7272f48f65bb4
Your Name <you@example.com>
2024-09-16 07:59:23 -0400

    [env] Add/Modify env scripts

--- setup_env.sh
+++ setup_env.sh
@@ -12,9 +12,17 @@ source djangoenv/bin/activate
 
 python3 -m pip install -U -r requirements.txt
 
+# Prepare les fichier de migration a partir des fichier python model.py
 # python3 manage.py makemigrations
+# Applique les changements en db
 # python3 manage.py migrate
 
+# python3 manage.py createsuperuser
+
 # python3 manage.py runserver
+
+# depuis le repertoire front end executer les commande suivante:
+# npm install
+# npm run build => build l'app react dans le repertoir build
 
 source ~/.bashrc
