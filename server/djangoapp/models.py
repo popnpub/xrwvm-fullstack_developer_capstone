d5a0dd48eee2f8dddc6c71cdcabcdc9f42e8686c
Your Name <you@example.com>
2024-09-24 08:26:32 -0400

    [autopep8] autopep8 -i: reformat file

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -39,9 +39,9 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2024,
-        validators=[MaxValueValidator(2024),
-                    MinValueValidator(2015)
-                    ])
-    
+                               validators=[MaxValueValidator(2024),
+                                           MinValueValidator(2015)
+                                           ])
+
     def __str__(self):
         return self.name
--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -11,11 +11,12 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
+
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params+key+"="+value+"&"
     request_url = backend_url+endpoint+"?"+params
     print("GET from {} ".format(request_url))
     try:
@@ -26,6 +27,7 @@ def get_request(endpoint, **kwargs):
     except Exception as e:
         # If any error occurs
         print(f"Network exception occurred: {e}")
+
 
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
@@ -41,7 +43,7 @@ def analyze_review_sentiments(text):
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
     except:
